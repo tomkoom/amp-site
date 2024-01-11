1c22eb67e30f7e986157c22d4069f4287b15567f
tomkoom <tomash.sugint@gmail.com>
2024-01-12 00:33:31 +0700

    add constants

---
+++ frontend/constants/_index.ts
@@ -0,0 +1,2 @@
+export const HOST_IC = "https://icp0.io"
+export const MY_LEDGER_CANISTER_ID = "fq7md-ayaaa-aaaag-abpea-cai"
--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -3,28 +3,25 @@ import { Principal } from "@dfinity/principal"
 import { AnonymousIdentity, HttpAgent, Actor } from "@dfinity/agent"
 import { idlFactory } from "../idl/icrc1_ledger"
 import { _SERVICE } from "../idl/icrc1_ledger_types"
+import { HOST_IC, MY_LEDGER_CANISTER_ID } from "../constants/_index"
 
 interface Metadata {
   name: string
   symbol: string
   fee: number
+  total_supply: number
 }
 
 const Home: FC = (): JSX.Element => {
   const [token, setToken] = useState<_SERVICE>()
   const [metadata, setMetadata] = useState<Metadata>()
 
-  const HOST_IC = "https://icp0.io"
-  const MY_LEDGER_CANISTER_ID = "fq7md-ayaaa-aaaag-abpea-cai"
-
   const initLedger = async (): Promise<void> => {
-    console.log(new AnonymousIdentity())
     const agentOptions = {
       identity: new AnonymousIdentity(),
       host: HOST_IC,
     }
     const agent = new HttpAgent(agentOptions)
-    console.log(agent)
 
     setToken(
       Actor.createActor(idlFactory, {
@@ -40,10 +37,14 @@ const Home: FC = (): JSX.Element => {
       name: "",
       symbol: "",
       fee: 0,
+      total_supply: 0,
     }
     await token.icrc1_name().then((res) => (metadata.name = res))
     await token.icrc1_symbol().then((res) => (metadata.symbol = res))
     await token.icrc1_fee().then((res) => (metadata.fee = Number(res)))
+    await token
+      .icrc1_total_supply()
+      .then((res) => (metadata.total_supply = Number(res)))
     setMetadata(metadata)
   }
 
